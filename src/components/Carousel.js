85685dd6d62df4a31a0979cbe4f5d00715e45dfe
Aimee Bojorquez <abojo079@me.com>
2020-03-06 21:25:23 -0500

    src

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -5,11 +5,11 @@ export default () => (
   <Carousel autoPlay>
     <div>
       <img class="materialboxed" src="https://i.cbc.ca/1.5170035.1560210566!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/hospital-doctor.jpg" alt="1"/>
-      <p className="legend">Find Doctors Near You</p>
+      <p className="legend" id="preview">Find Doctors Near You</p>
     </div>
     <div>
-      <img class='pharmacy'src="https://media.gettyimages.com/photos/happy-pharmacist-working-at-a-drugstore-picture-id610573018" alt="2" />
-      <p className="legend">Find Closest Pharmacies and Compare Prescription Costs</p>
+      <img class="img-responsive" src="https://media.gettyimages.com/photos/happy-pharmacist-working-at-a-drugstore-picture-id610573018" alt="2" />
+      <p className="legend" id="preview">Find Closest Pharmacies and Compare Prescription Costs</p>
     </div>
     
   </Carousel>
