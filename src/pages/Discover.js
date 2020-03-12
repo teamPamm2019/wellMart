c3bd6263160b22f97ae8164a6ea3b6e4b5adc07c
Aimee Bojorquez <abojo079@me.com>
2020-03-11 22:22:45 -0400

    src

--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -1,7 +1,9 @@
 import Map from '../googleMaps/googlemaps';
 import React from 'react';
 import {Component} from 'react';
-
+import searchDrRes from "../components/searchDocResults/searchDrRes";
+import searchDr from '../components/SearchDoc/searchDr';
+import Container from "../components/Container";
 
 
 class Discover extends Component {
@@ -28,6 +30,15 @@ class Discover extends Component {
   render() {
     return (
       <>
+ <Container style={{ minHeight: "20%" }}>
+          <h5 className="left-align">Search for Doctor in your Area:</h5>
+         
+          <searchDr
+            handleFormSubmit={this.handleFormSubmit}
+            handleInputChange={this.handleInputChange}
+            breeds={this.state.breeds}
+          />
+        </Container>
 
         <Map
           id="myMap"
@@ -38,6 +49,15 @@ class Discover extends Component {
 
           }}
           onMapLoad = {this.handleMapLoad}
+
+              
+
+
+/>
+       
+
+
+
         />   
        </>
 
