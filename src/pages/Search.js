58a896d69eccc367d1f1185c642a6db76d01c800
Aimee Bojorquez <abojo079@me.com>
2020-03-11 21:26:12 -0400

    src

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,7 +3,6 @@ import API from "../utils/API";
 import Container from "../components/Container";
 import SearchForm from "../components/SearchForm";
 import SearchResults from "../components/SearchResults";
-import Alert from "../components/Alert";
 
 class Search extends Component {
   state = {
@@ -40,12 +39,7 @@ class Search extends Component {
       <div>
         <Container style={{ minHeight: "80%" }}>
           <h1 className="text-center">Search By Breed!</h1>
-          <Alert
-            type="danger"
-            style={{ opacity: this.state.error ? 1 : 0, marginBottom: 10 }}
-          >
-            {this.state.error}
-          </Alert>
+         
           <SearchForm
             handleFormSubmit={this.handleFormSubmit}
             handleInputChange={this.handleInputChange}
---
+++ src/pages/googlemaps.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+require('dotenv').config();
+
+
+
+class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.onScriptLoad = this.onScriptLoad.bind(this)
+  }
+
+  onScriptLoad() {
+    const map = new window.google.maps.Map(document.getElementById(this.props.id),this.props.options);
+    this.props.onMapLoad(map)
+  }
+
+
+
+  componentDidMount() {
+
+const APIkey= process.env.REACT_APP_GOOGLE_KEY;
+
+    if (!window.google) {
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = "https://maps.google.com/maps/api/js?key=" + APIkey;
+      script.id = 'googleMaps';
+      document.body.appendChild(script);
+      script.addEventListener('load', e => {
+        this.onScriptLoad()
+      })
+    } 
+    else {
+      this.onScriptLoad()
+    }
+  }
+
+  render() {
+    return (
+      <div style={{ width: 500, height: 500 }} id={this.props.id} />
+    );
+  }
+}
+
+export default Map;
