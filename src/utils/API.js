85b8b37d6a8bf9d91d64dbb27456aab64afd19fe
Aimee Bojorquez <abojo079@me.com>
2020-03-13 16:36:03 -0400

    src

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 // Export an object containing methods we'll use for accessing the Dog.Ceo API
+const APIkey2= process.env.REACT_APP_GOOGLE_KEY;
 
 export default {
 
   getDoctor: function(doctor) {
-    return axios.get("https://api.betterdoctor.com/2016-03-01/practices?location=" + doctor + "/location");
+    return axios.get("https://api.betterdoctor.com/2016-03-01/practices?location=" + doctor + "/location" + APIkey2);
   },
   getBaseBreedsList: function() {
     return axios.get("https://dog.ceo/api/breeds/list");
