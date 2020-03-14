2941e30252353e17a359762e0398634d0be24f29
Miguel Fernandez <miguelfernandez023@gmail.com>
2020-03-14 18:38:35 -0400

    working branch

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,37 +1,95 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Discover from "./pages/Discover";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
 
-import Login from "./components/Login";
-import About from "./pages/About";
-import Search from "./pages/Search";
-import Navbar from "./components/Navbar";
-import SubFooter from "./components/SubFooter";
-import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper";
-import News from "./pages/News";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { Provider } from "react-redux";
+import store from "./store";
 
-function App() {
-  return (
-    <Wrapper>
-      <Router>
-        <div>
-          <Navbar />
+import Navbar from "./components/layout/NavBar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import Dashboard from "./components/dashboard/Dashboard";
 
-          <Route exact path="/" component={About} />
-          <Route exact path="/Login" component={Login} />
+import "./App.css";
 
-          <Route exact path="/Doctor" component={About} />
-          <Route exact path="/discover" component={Discover} />
-          <Route exact path="/search" component={Search} />
-          <Route exact path="/news" component={News} />
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
 
-          <SubFooter />
-          <Footer />
-        </div>
-      </Router>
-    </Wrapper>
-  );
+    // Redirect to login
+    window.location.href = "./login";
+  }
 }
-
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <Switch>
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            </Switch>
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
+}
 export default App;
+
+// import React from "react";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+// import Discover from "./pages/Discover";
+
+// import Login from "./components/Login";
+// import About from "./pages/About";
+// import Search from "./pages/Search";
+// import Navbar from "./components/Navbar";
+// import SubFooter from "./components/SubFooter";
+// import Footer from "./components/Footer";
+// import Wrapper from "./components/Wrapper";
+// import News from "./pages/News";
+
+// function App() {
+//   return (
+//     <Wrapper>
+//       <Router>
+//         <div>
+//           <Navbar />
+
+//           <Route exact path="/" component={About} />
+//           <Route exact path="/Login" component={Login} />
+
+//           <Route exact path="/Doctor" component={About} />
+//           <Route exact path="/discover" component={Discover} />
+//           <Route exact path="/search" component={Search} />
+//           <Route exact path="/news" component={News} />
+
+//           <SubFooter />
+//           <Footer />
+//         </div>
+//       </Router>
+//     </Wrapper>
+//   );
+// }
+
+// export default App;
